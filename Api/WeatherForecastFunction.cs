ce3763571be93717a7d24ba239e650af634e8672
Haifischnikez <89863681+Haifischnikez@users.noreply.github.com>
2021-09-20 18:38:41 +0200

    Update WeatherForecastFunction.cs

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -10,27 +10,64 @@ using BlazorApp.Shared;
 
 namespace BlazorApp.Api
 {
-    public static class WeatherForecastFunction
+    public class WeatherForecastService
     {
-        private static string GetSummary(int temp)
+        public Task<WeatherForecast[]> stadt(DateTime startDate, String stadt)
         {
-            var summary = "Mild";
+            var client = new OpenWeatherAPI.API("b9ae2332feb67e597cae025895d7b866");
+            var results_stadt = client.Query(stadt);
 
-            if (temp >= 32)
+            return Task.FromResult(Enumerable.Range(1, 1).Select(index => new WeatherForecast
             {
-                summary = "Hot";
-            }
-            else if (temp <= 16 && temp > 0)
-            {
-                summary = "Cold";
-            }
-            else if (temp <= 0)
-            {
-                summary = "Freezing";
-            }
 
-            return summary;
+                Location = stadt,
+                Date = startDate.AddDays(index),
+                TemperatureC = results_stadt.Main.Temperature.CelsiusCurrent,
+                Humdity = results_stadt.Main.Humdity,
+                Pressure = results_stadt.Main.Pressure,
+                //H3 = results_stadt.Rain.H3,
+                Clouds = results_stadt.Clouds.All,
+                Wind_Speed = results_stadt.Wind.SpeedMetersPerSecond,
+                Wind_Direction = results_stadt.Wind.Degree
+
+            }).ToArray());
         }
+
+
+        public WeatherForecast[] stadt_arrow(String stadt_user)
+        {
+            var task = stadt(DateTime.UtcNow, stadt_user);
+            WeatherForecast[] result = task.Result;
+            return result;
+        }
+
+
+        public WeatherForecast lol(String stadt)
+        {
+            var client = new OpenWeatherAPI.API("b9ae2332feb67e597cae025895d7b866");
+            var results_stadt = client.Query(stadt);
+
+
+            WeatherForecast temp = new WeatherForecast()
+            {
+                Location = stadt,
+                Date = DateTime.UtcNow,
+                TemperatureC = results_stadt.Main.Temperature.CelsiusCurrent,
+                Humdity = results_stadt.Main.Humdity,
+                Pressure = results_stadt.Main.Pressure,
+                //H3 = results_stadt.Rain.H3,
+                Clouds = results_stadt.Clouds.All,
+                Wind_Speed = results_stadt.Wind.SpeedMetersPerSecond,
+                Wind_Direction = results_stadt.Wind.Degree,
+                //Wind_Direction_Str = results_stadt.Wind.DirectionEnum.Direction
+
+            };
+
+            return temp;
+
+        }
+    }
+}
 
         [FunctionName("WeatherForecast")]
         public static IActionResult Run(
